ff5d01244af80f3ed86ae212090c572f299c83c6
marcocentonze <marco.centonze27@gmail.com>
2023-08-27 18:44:46 +0200

    final commit

--- assets/main.js
+++ assets/main.js
@@ -10,8 +10,38 @@ Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
 
+/* Strumenti:
+    - const
+    - log
+    - for
+    - log
+    - if/else if/else if/else
+    - stampo in pagina
+*/
 
-//stampare tutti i numero da 0 a 100
-for (let i = 0; i <100; i++){
+// Ottenere "container" dal DOM
+const container = document.getElementById('container');
+console.log(container);
 
+// Ciclo da 1 a 100
+for (let i = 1; i < 101; i++) {
+
+    // Creare un nuovo elemento div per ciascun numero che si crea
+    let element = document.createElement('div');
+    //faccio i controlli per vedere se un numoro è fizz...
+    if (i % 3 == 0 && i % 5 == 0) {
+        element.innerHTML = 'FizzBuzz'; //inserisco il contenuto fizzbuzz..
+        element.className = 'circle fizzBuzz'; //assegno la classe...
+    } else if (i % 3 == 0) {
+        element.innerHTML = 'Fizz';
+        element.className = 'circle fizz';
+    } else if (i % 5 == 0) {
+        element.innerHTML = 'Buzz';
+        element.className = 'circle buzz';
+    } else {
+        element.innerHTML = i;
+        element.className = 'circle num';
+    }
+    //scrivo in pagina
+    container.append(element);
 }
